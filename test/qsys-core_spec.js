c982d4ebea6bfc3b262ece9bee8f4d4c87fc7a9a
dudest <34729881+dudest@users.noreply.github.com>
2023-07-22 19:24:44 -0400

    Update qsys-core_spec.js

--- test/qsys-core_spec.js
+++ test/qsys-core_spec.js
@@ -1,6 +1,7 @@
 var should = require("should");
 var helper = require("node-red-node-test-helper");
 var qsysCore = require("../lib/qsys-core.js");
+var qsysControlSet = require("../lib/qsys-ControlSet.js");
 
 helper.init(require.resolve('node-red'));
 
@@ -13,18 +14,5 @@ describe('qsys-core node', function () {
     afterEach((done) => {
         helper.unload();
         helper.stopServer(done);
-    });
-
-    it('should be loaded', (done) => {
-        var flow = [{ id: "n1", type: "qsys-core", host: "127.0.0.1" }];
-        helper.load(qsysCore, flow, () => {
-            var n1 = helper.getNode("n1");
-            try {
-                n1.should.have.property('host', '127.0.0.1');
-                done();
-            } catch (err) {
-                done(err);
-            }
-        });
     });
 });
